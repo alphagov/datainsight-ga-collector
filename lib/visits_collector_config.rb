cd2e7e07b910dfe736653b887eff41cddab9fc82
Johannes Thoenes <johannes.thoenes@digital.cabinet-office.com>
2012-08-08 17:50:10 +0100

    Week result set should contain year as well.
    @author Johannes

--- lib/visits_collector_config.rb
+++ lib/visits_collector_config.rb
@@ -2,7 +2,7 @@ module VisitsCollectorConfig
     GOOGLE_ANALYTICS_URL_ID = "ga:53872948"
     METRIC = "ga:visits"
     METRIC_NAME = "visits"
-    DIMENSION = "ga:week"
+    DIMENSION = "ga:week, ga:year"
     SOURCE = "Google Analytics Reach"
     FILTER = ""
     UNIT = "visits"
