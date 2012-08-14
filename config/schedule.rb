b39909e42ad0c27e604d3703a5cfc2c7efc562f9
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-08-14 13:38:21 +0100

    Update whenever schedule.

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,3 +5,8 @@ end
 every :sunday, :at => '1am' do
   command "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && bundle exec bin/collector --config=WeeklyUniqueVisitors broadcast"
 end
+
+# Ten minutes after every full hour
+every :hour, :at => '00:10' do
+  command "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && bundle exec bin/collector --config=WeeklyUniqueVisitors broadcast"
+end
