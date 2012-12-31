0b824ca515c6f1f81b88990a57720871d7bccf34
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-12-31 13:02:48 +0000

    Fix import paths in spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,12 @@
 require 'bundler/setup'
 Bundler.require
 
-Dir[File.expand_path(File.join(File.dirname(__FILE__), "../../lib/**/*.rb"))].each {|f| require f}
+Dir[File.expand_path(File.join(File.dirname(__FILE__), "../lib/**/*.rb"))].each {|f| require f}
 
 require "json"
 
 def load_json(filename)
-  JSON.parse(File.read(File.join(File.dirname(__FILE__), "../data", filename)))
+  JSON.parse(File.read(File.join(File.dirname(__FILE__), "data", filename)))
 end
 
 class DummyConfig
