ed3ebb7431cb7cf4bb1b7d4341eebb7ed9968731
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-12-31 16:07:50 +0000

    Stub out File.exists? for refresh token
    So that the oauth refresh token get's generated correctly.
    There is an issue where rspec stub stubs all calls to File.exists?
    so there is a catch all first that delegates the call to File.exist?
    This isn't a nice way of handling it but I can't get a better one to
    work.

--- spec/integration/spec_helper.rb
+++ spec/integration/spec_helper.rb
@@ -7,10 +7,18 @@ FakeWeb.allow_net_connect = false
 def stub_credentials
   YAML.stub!(:load_file)
       .with("/etc/govuk/datainsight/google_credentials.yml")
-      .and_return({:google_client_id => "", :google_client_secret => ""})
+      .and_return({:google_client_id => "blahblah", :google_client_secret => "blahblah"})
   YAML.stub!(:load_file)
       .with("/var/lib/govuk/datainsight/google-analytics-token.yml")
-      .and_return({:refresh_token => ""})
+      .and_return({:refresh_token => "blahblah"})
+  ## you can't only stub requests with certain arguments so
+  ## this proxies other exists? calls to exist?
+  File.stub!(:exists?) do |path|
+    File.exist?(path)
+  end
+  File.stub!(:exists?)
+      .with("/var/lib/govuk/datainsight/google-analytics-token.yml")
+      .and_return(true)
 end
 
 def register_oauth_refresh
