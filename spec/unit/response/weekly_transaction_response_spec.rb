9ecf4be4b329e7127fdcf9c85a791d4748a24d61
Gareth Rogers <grogers@thoughtworks.com>
2013-01-07 12:59:09 +0000

    Stubs out response and spec.
    - @gtrogers

--- spec/unit/response/weekly_transaction_response_spec.rb
+++ spec/unit/response/weekly_transaction_response_spec.rb
@@ -1,9 +1,7 @@
 require_relative "../../spec_helper"
 
-include "weekly entry/success reponse" do
+describe "weekly entry/success reponse" do
 
-  it "should have an array of messages" do
-    @response.messages.should be_an(Array)
-  end
+  it "should have an array of messages"
 
 end
