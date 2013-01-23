5790be6b0ac4d8736a366e44475c900f62989e10
Pawel Badenski <pawel.badenski@thoughtworks.com>
2013-01-23 13:58:47 +0000

    No need to have these tests separately
    GA Collector class tests merged to be in the same file
    
    @maxfliri
    @pbadenski

--- spec/unit/collector_spec.rb
+++ spec/unit/collector_spec.rb
@@ -8,6 +8,35 @@ describe GoogleAnalytics::Collector do
       GoogleAnalytics::Config::WeeklyVisitors.new(Date.today - 6, Date.today)
     ]
 
-    lambda {GoogleAnalytics::Collector.new("any-code", configs)}.should raise_exception
+    lambda { GoogleAnalytics::Collector.new("any-code", configs) }.should raise_exception
+  end
+
+  describe "Collection" do
+    module GoogleAnalytics
+      class Collector
+        public :collect_messages
+      end
+    end
+
+    class MockResponse
+      def initialize data, _
+        @data = data
+      end
+
+      def messages
+        @data[:data].is_a?(Array) ? @data[:data] : [@data[:data]]
+      end
+    end
+    it "should collect messages for one config" do
+      config = mock(:config)
+      config.should_receive(:response_type).and_return(MockResponse)
+      collector = GoogleAnalytics::Collector.new(nil, [config])
+
+      collector.should_receive(:collect).and_return({ :data => :X })
+
+      messages = collector.collect_messages(config)
+      messages.should have(1).item
+      messages.should include(:X)
+    end
   end
 end
