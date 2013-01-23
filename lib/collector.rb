554f372dd2b8cdeb1fb959c6846895013b0453e1
Pawel Badenski <pawel.badenski@thoughtworks.com>
2013-01-23 13:57:01 +0000

    "broadcast" is no longer neccesary
    Broadcast functionality is being provided by a gem
    
    @maxfliri
    @pbadenski

--- lib/collector.rb
+++ lib/collector.rb
@@ -29,24 +29,6 @@ module GoogleAnalytics
       messages
     end
 
-    def broadcast
-      begin
-        logger.info { "Starting to collect google analytics data ..." }
-        Bunny.run(ENV['AMQP']) do |client|
-          exchange = client.exchange("datainsight", :type => :topic)
-          @configs.each do |config|
-            messages = collect_messages(config)
-            messages.each do |message|
-              exchange.publish(message.to_json, :key => config.amqp_topic)
-            end
-          end
-        end
-        logger.info { "Collected the google analytics data." }
-      rescue => e
-        logger.error { e }
-      end
-    end
-
     private
     def collect_messages(config)
       collect_response(config).messages
