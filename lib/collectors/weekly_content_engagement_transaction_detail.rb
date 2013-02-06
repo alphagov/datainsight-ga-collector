9eaf151abf88a133ea53812e776ecbdeabbc275e
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-02-06 16:51:13 +0000

    Add collector config for transaction engagement detail
    @maxfliri
    @robyoung

--- lib/collectors/weekly_content_engagement_transaction_detail.rb
+++ lib/collectors/weekly_content_engagement_transaction_detail.rb
@@ -2,18 +2,24 @@ require_relative "../response/weekly_content_engagement_detail_response"
 
 module GoogleAnalytics
   module Config
-    class WeeklyContentEngagementDetail < Base
+    class WeeklyContentEngagementTransactionDetail < Base
       include WeeklyCollector
 
-      GOOGLE_ANALYTICS_URL_ID = "ga:53872948"
+      GOOGLE_ANALYTICS_URL_ID = %w(ga:53872948 ga:61976178)
       AMQP_TOPIC = "google_analytics.content_engagement.weekly"
       SITE_KEY = "govuk"
 
       DIMENSION = "ga:eventCategory,ga:eventAction,ga:eventLabel"
       METRIC = "ga:totalEvents"
       CATEGORY_PREFIX = "MS_"
-      FILTERS= "ga:eventCategory=~^#{CATEGORY_PREFIX}.*;ga:eventCategory!=MS_transaction"
+      FILTERS= "ga:eventCategory==MS_transaction"
       RESPONSE_TYPE = GoogleAnalytics::WeeklyContentEngagementDetailResponse
+
+      def analytics_parameters()
+        self.class::GOOGLE_ANALYTICS_URL_ID.map do |id|
+          build_parameters_for(id: id)
+        end
+      end
     end
   end
 end
