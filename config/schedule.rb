6d54ec146a817be680df4c46ce722278a69a1aee
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-03-12 09:39:43 +0000

    Split Sunday morning collection into groups
    Avoids hitting Quotas when everything starts at once.

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,16 +6,21 @@ set :output, {
 job_type :collector, "cd :path && RACK_ENV=:environment bundle exec collector --config=:config --days_ago=:days_ago :task :output"
 
 
-every :sunday, :at => '5am' do
+every :sunday, :at => '05:00' do
   collector "broadcast", :config => "WeeklyVisits", :days_ago => 0
   collector "broadcast", :config => "WeeklyVisitors", :days_ago => 0
+  collector "broadcast", :config => "InsideGovWeeklyVisitors", :days_ago => 0
+  collector "broadcast", :config => "InsideGovWeeklyPolicyEntries", :days_ago => 0
   collector "broadcast", :config => "WeeklyContentEngagement", :days_ago => 0
+end
+
+every :sunday, :at => '05:10' do
   collector "broadcast", :config => "WeeklyContentEngagementDetail", :days_ago => 0
   collector "broadcast", :config => "WeeklyContentEngagementTransaction", :days_ago => 0
   collector "broadcast", :config => "WeeklyContentEngagementTransactionDetail", :days_ago => 0
+end
 
-  collector "broadcast", :config => "InsideGovWeeklyVisitors", :days_ago => 0
-  collector "broadcast", :config => "InsideGovWeeklyPolicyEntries", :days_ago => 0
+every :sunday, :at => '05:20' do
   collector "broadcast", :config => "InsideGovWeeklyContentEngagement", :days_ago => 0
   collector "broadcast", :config => "InsideGovWeeklyContentEngagementDetail", :days_ago => 0
 end
